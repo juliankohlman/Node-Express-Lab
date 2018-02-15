18f7a94ccf92ba9d1c9a631408d48f7aef9988d8
Julian Kohlman <juliankohlman@gmail.com>
2018-02-14 19:15:23 -0500

    Code and all functionality working fine only 6/14 tests passing

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const STATUS_USER_ERROR = 422;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 let posts = [];
-let postId = 0
+let postId = 0;
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
@@ -17,24 +17,24 @@ server.get('/posts', (req, res) => {
   const term = req.query.term;
   console.log(term);
   if (req.query.term) {
-    res.json({ post : posts.filter(post => post.title.includes(term) || post.contents.includes(term))})
+    res.json({ post: posts.filter(post => post.title.includes(term) || post.contents.includes(term)) });
   } else {
-    res.json({posts});
+    res.json({ posts });
   }
-})
+});
 
 server.post('/posts', (req, res) => {
   // validate req.body -> /title/contents if any of them are blank => ERROR
   if (!req.body.title || !req.body.contents) {
-    res.status(STATUS_USER_ERROR).json({ error: 'Please provide both a title and content in the body'})
+    res.status(STATUS_USER_ERROR).json({ error: 'Please provide both a title and content in the body' });
     return;
   }
-  req.body['id'] = postId
+  req.body.id = postId;
   postId++;
-  console.log(req.body)
-  posts.push(req.body)
-  res.json(req.body.id)
-})
+  console.log(req.body);
+  posts.push(req.body);
+  res.json(req.body.id);
+});
 
 server.put('/posts', (req, res) => {
   // if id doesn't belong to a post => ERROR
@@ -42,31 +42,28 @@ server.put('/posts', (req, res) => {
   // posts.push({})
   // console.log(Object.values(req.body))
   if (Object.values(req.body).includes('')) {
-    res.status(STATUS_USER_ERROR).json({ error: 'Must provide valid id, title, and contents'});
+    res.status(STATUS_USER_ERROR).json({ error: 'Must provide valid id, title, and contents' });
     return;
   }
-  console.log(posts.filter(p => req.body.id === p.id).length)
+  console.log(posts.filter(p => req.body.id === p.id).length);
   if (!posts.filter(p => req.body.id === p.id).length) {
-    res.status(STATUS_USER_ERROR).json({ error: `The post ID ${req.body.id} does not exist`})
-  }
-  else {
-    // modify the given id updating its title and contents respond with updated post obj with a
-    // res.json
-    posts = posts.map(p => req.body.id === p.id ? req.body : p);
+    res.status(STATUS_USER_ERROR).json({ error: `The post ID ${req.body.id} does not exist` });
+  } else {
+    posts = posts.map((p) => {
+      return req.body.id === p.id ? req.body : p;
+    });
     console.log(posts);
     res.json(posts.find(p => req.body.id === p.id));
-    // res.json()
   }
-  // res.send('PUT REQUEST')
-})
+});
 
 server.delete('/posts', (req, res) => {
   if (posts.find(p => req.body.id === p.id)) {
-    posts = posts.filter(p => req.body.id !== p.id)
+    posts = posts.filter(p => req.body.id !== p.id);
     res.json({ success: true });
   } else {
-    res.status(STATUS_USER_ERROR).json({ error: `Cannot delete ID # ${req.body.id} because it does not exist.`})
+    res.status(STATUS_USER_ERROR).json({ error: `Cannot delete ID # ${req.body.id} because it does not exist.` });
   }
-})
+});
 
 module.exports = { posts, server };
